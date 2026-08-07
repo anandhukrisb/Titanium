402d7285496e55cb1d3c244817e06f0d1c119b65
anandhukrisb <anandhukrishna10b@gmail.com>
2026-08-07 15:40:55 +0530

    Wrote my first harder Hello world!

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,56 @@
+org 0X7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+	jmp main
+
+;Prints a string to the screen
+;Params:
+;	-ds:si points to string
+
+puts:
+	push si
+	push ax
+
+.loop:
+	lodsb	;loads next character in al
+	or al, al	;is the next character null?
+	jz .done
+
+	mov ah, 0x0e	;bios interrupt called
+	mov bh, 0
+	int 0x10
+	
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
 
 main:
+	; setup data segments
+
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+
+	; setup stack
+	mov ss, ax
+	mov sp,0X7C00
+
+	;prints the message
+	mov si, msg_hello
+	call puts
+
 	hlt
 
 .halt:
 	jmp .halt
+
+msg_hello: db 'Hello World!', ENDL, 0
 
 times 510-($-$$) db 0
 
